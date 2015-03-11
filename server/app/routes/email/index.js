0a1f4924e12d8580a4bd67f3db4bb73f37d303d5
Colin VanLang <colinvanlang@gmail.com>
2015-03-11 18:08:53 -0400

    save

--- server/app/routes/email/index.js
+++ server/app/routes/email/index.js
@@ -1,9 +1,25 @@
 'use strict';
-var VIDEOS = require('./videos.json');
-
 var router = require('express').Router();
+
+var Google = require('googleapis');
+var Gmail = Google.gmail('v1');
+
+var mongoose = require('mongoose');
+var Email = mongoose.model('Email');
+
 module.exports = router;
 
-router.get('/videos', function (req, res) {
-    res.send(VIDEOS);
+
+router.get('/', function(res, req, send) {
+	// if pulling from database
+	Email.find().exec().then(function(emails) {
+		emails = emails.map(function(emailObj) {
+			return emailObj.email;
+		});
+		res.json(emails);
+	});
+
+
+	// if pulling from gmail
+	gmail
 });
