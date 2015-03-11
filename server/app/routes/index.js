c3e4f3cc81c6bcd57d96703df72a9624219f91c3
Colin VanLang <colinvanlang@gmail.com>
2015-03-11 17:49:45 -0400

    bringing in stuff

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,8 +2,7 @@
 var router = require('express').Router();
 module.exports = router;
 
-router.use('/tutorial', require('./tutorial'));
-router.use('/members', require('./members'));
+router.use('/email', require('./email'));
 
 // Make sure this is after all of
 // the registered routes!
