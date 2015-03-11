abd95d7a552347e01233654640bea553fbba6935
Colin VanLang <colinvanlang@gmail.com>
2015-03-11 18:20:22 -0400

    route beginning

--- server/app/routes/email/index.js
+++ server/app/routes/email/index.js
@@ -10,6 +10,9 @@ var Email = mongoose.model('Email');
 module.exports = router;
 
 
+var emailLimit = 10000;
+var userId = 'colinvanlang@gmail.com';
+
 router.get('/', function(res, req, send) {
 	// if pulling from database
 	Email.find().exec().then(function(emails) {
@@ -21,5 +24,13 @@ router.get('/', function(res, req, send) {
 
 
 	// if pulling from gmail
-	gmail
+	var getEmails = function(pageToken) {
+		Gmail.users.message.list({userId: userId}, function(err, res) {
+			emailLimit -= res.resultSizeEstimate;
+		});
+	}
+	Gmail.users.messages.list({userId: 'colinvanlang@gmail.com'}).then(function(emails) {
+		emailLimit -= emails.size;
+		if emailLimit > 0 //do again
+	})
 });
