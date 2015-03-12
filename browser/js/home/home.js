1d54822b1e18aa1bb1838684f325f91cca07b18b
Vincenzo Merolla <vincenzomerolla@aol.com>
2015-03-12 10:58:11 -0400

    Email data coming back

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -2,6 +2,19 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeCtrl',
+        resolve: {
+          user: function(AuthService) {
+            return AuthService.getLoggedInUser();
+          },
+          emails: function(Email, user) {
+            return user ? Email.query().$promise : $q.when([]);
+          }
+        }
     });
+});
+
+app.controller('HomeCtrl', function ($scope, emails){
+  $scope.emails = emails;
 });
