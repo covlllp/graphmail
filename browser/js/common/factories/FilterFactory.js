3366a63d87868d2f3e46f7c92877427a4284bd83
Colin VanLang <colinvanlang@gmail.com>
2015-03-12 22:10:29 -0400

    done reorganizing

--- browser/js/common/factories/FilterFactory.js
+++ browser/js/common/factories/FilterFactory.js
@@ -8,18 +8,6 @@ app.factory('FilterFactory', function(
 		chartEmails: []
 	};
 
-	// var toggleHangouts = function(bool) {
-	// 	// true if show, false if hide
-	// 	return function() {
-	// 		factory.data.chartEmails = factory.data.chartEmails.filter(function(email) {
-	// 			var isHangout = email.payload.headers[1];
-	// 			return bool ? !isHangout : isHangout;
-	// 		});
-	// 	};
-	// };
-
-
-
 	factory.filterFunctions = {
 		'Hide Hangouts': filterHangouts(false),
 		'Only Hangouts': filterHangouts(true)
@@ -33,7 +21,8 @@ app.factory('FilterFactory', function(
 		factory.resetEmails();
 		for (var key in filterObj) {
 			if (filterObj[key]) {
-				factory.filterFunctions[key](factory.data.chartEmails);
+				factory.data.chartEmails
+					= factory.filterFunctions[key](factory.data.chartEmails);
 			}
 		}
 	};
--- browser/js/common/factories/filters/filterHangouts.js
+++ browser/js/common/factories/filters/filterHangouts.js
@@ -1,7 +1,7 @@
 app.factory('filterHangouts', function() {
 	return function(bool) {
 		return function(emails) {
-			emails = emails.filter(function(email) {
+			return emails.filter(function(email) {
 				var isHangout = email.payload.headers[1];
 				return bool ? !isHangout : isHangout;
 			});
