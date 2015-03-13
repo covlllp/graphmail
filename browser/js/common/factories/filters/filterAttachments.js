70d09b4f18f4eb0fc973f3488123a85077133dab
Colin VanLang <colinvanlang@gmail.com>
2015-03-13 15:54:35 -0400

    sent vs receive done

--- browser/js/common/factories/filters/filterAttachments.js
+++ browser/js/common/factories/filters/filterAttachments.js
@@ -1,12 +1,16 @@
 app.factory('filterAttachments', function() {
+	function searchForAttachment(part) {
+		if (part.hasOwnProperty('parts')) {
+			return part.parts.some(searchForAttachment);
+		} else return part.body.hasOwnProperty('attachmentId');
+	}
+
 	return function(bool) {
 		// true with attachments, false without
 		return function(emails) {
 			return emails.filter(function(email) {
 				var hasAttachment = email.payload.parts
-									&& email.payload.parts.some(function(part) {
-										return part.body.hasOwnProperty('attachmentId');
-									});
+					&& email.payload.parts.some(searchForAttachment);
 				return bool ? hasAttachment : !hasAttachment;
 			});
 		};
