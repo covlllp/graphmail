03d4b28c44069c9bc5c55f2c1ad68eb034c3c325
Colin VanLang <colinvanlang@gmail.com>
2015-03-13 15:42:56 -0400

    send or receive done

--- browser/js/common/factories/types/typeAttachments.js
+++ browser/js/common/factories/types/typeAttachments.js
@@ -1,8 +1,8 @@
 app.factory('typeAttachments', function(filterAttachments) {
 	return function(emails) {
 		var house = {};
-	    house.Attachment = filterAttachments(true)(emails);
-	    house['Not Attachment'] = filterAttachments(false)(emails);
-	    return house;
+    house.Attachment = filterAttachments(true)(emails);
+    house['Not Attachment'] = filterAttachments(false)(emails);
+    return house;
 	};
 });
