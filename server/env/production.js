e9d7e1e43ebbd705a5f046971ce224de9db1830a
Colin VanLang <colinvanlang@gmail.com>
2015-03-20 00:10:41 -0400

    savepoint

--- server/env/production.js
+++ server/env/production.js
@@ -1,19 +1,12 @@
 module.exports = {
     "DATABASE_URI": process.env.MONGOLAB_URI,
     "SESSION_SECRET": process.env.SESSION_SECRET,
-    "TWITTER": {
-        "consumerKey": process.env.TWITTER_CONSUMER_KEY,
-        "consumerSecret": process.env.TWITTER_CONSUMER_SECRET,
-        "callbackUrl": process.env.TWITTER_CALLBACK
-    },
-    "FACEBOOK": {
-        "clientID": process.env.FACEBOOK_APP_ID,
-        "clientSecret": process.env.FACEBOOK_CLIENT_SECRET,
-        "callbackURL": process.env.FACEBOOK_CALLBACK_URL
-    },
     "GOOGLE": {
         "clientID": process.env.GOOGLE_CLIENT_ID,
         "clientSecret": process.env.GOOGLE_CLIENT_SECRET,
-        "callbackURL": process.env.CALLBACK_URL
+        "callbackURL": process.env.CALLBACK_URL,
+        "gmailClientID": process.env.GMAIL_GOOGLE_CLIENT_ID,
+        "gmailClientSecret": process.env.GMAIL_GOOGLE_CLIENT_SECRET,
+        "gmailCallbackURL": process.env.GMAIL_CALLBACK_URL
     }
 };
